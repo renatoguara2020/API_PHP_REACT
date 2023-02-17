e91dbc8a103ecb36420c9994597e7896f664ab5e
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-17 08:33:58 -0300

    ok

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
 
-include_once '/connection.php';
+include_once 'connection.php';
+
+
+
+function resposta($codigo, $ok, $mensagem){
+
+
+}
 
 header("Access-Control-Allow-Origin: * ");
 header("Access-Control-Allow-Headers: * ");
@@ -12,10 +19,13 @@ header("Content-Type: application/json");
 
 
 
-if(! $body)
-
-echo('Corpo da requisição não encontrado!!');
 
 $body = file_get_contents('php://input');
 
 $body = json_decode($body);
+
+if(! $body){
+
+    echo json_encode('Corpo da requisição não encontrado!!');
+    die;
+    }
