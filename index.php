78363fc923d6c04a57331b9495054eb02e79f1bc
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-17 08:46:56 -0300

    OK

--- index.php
+++ index.php
@@ -5,14 +5,25 @@ include_once 'connection.php';
 
 
 
-function resposta($codigo, $ok, $mensagem){
+function resposta($codigo, $ok, $msg){
 
+    header("Access-Control-Allow-Origin: * ");
+    header("Access-Control-Allow-Headers: * ");
+    header("Content-Type: application/json");
 
+    http_response_code($codigo);
+
+    echo json_encode([
+
+        'ok' => $ok,
+        'msg' => $msg,
+    ]);
+
+    die;
 }
 
-header("Access-Control-Allow-Origin: * ");
-header("Access-Control-Allow-Headers: * ");
-header("Content-Type: application/json");
+
+
 
 
 //echo json_encode(["resposta" => "ok"]);
@@ -22,10 +33,14 @@ header("Content-Type: application/json");
 
 $body = file_get_contents('php://input');
 
+
+
+if(! $body)
+
+resposta(400, false, "Corpo da requisição não encontrado");
+
 $body = json_decode($body);
 
-if(! $body){
-
-    echo json_encode('Corpo da requisição não encontrado!!');
-    die;
-    }
+$body->nome = filter_var($body->nome, FILTER_SANITIZE_STRING);
+$body->email = filter_var($body->email, FILTER_SANITIZE_STRING);
+$body->msg = filter_var($body->msg, FILTER_SANITIZE_STRING);
